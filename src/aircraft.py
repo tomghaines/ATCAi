5b054f68cb751ef1d2c36d0ef7b494259a1b825a
tomghaines <tomhaines001@gmail.com>
2024-08-20 16:29:08 +0100

    formatted TestAircraft class for improved readability

--- src/aircraft.py
+++ src/aircraft.py
@@ -5,6 +5,17 @@ def GetMach(v):
     M = c / v
     return M
 
-TestAircraft = NewAircraft(aircraft_type="Boeing 737 MAX 8-200 (B38M)", registration="EI-IHT", barometric_altitude=37000, GPS_altitude=37000, track=77, ground_speed=502, airspeed=502, mach=GetMach(502), latitude=51.75723, longitude=-0.28253)
+TestAircraft = NewAircraft(
+    aircraft_type="Boeing 737 MAX 8-200 (B38M)",
+    registration="EI-IHT",
+    barometric_altitude=37000,
+    GPS_altitude=37000,
+    track=77,
+    ground_speed=502,
+    airspeed=502,
+    mach=GetMach(502),
+    latitude=51.75723,
+    longitude=-0.28253
+)
 
 print(TestAircraft)
