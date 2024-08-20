756485c56a09b3c63657f8964948d0e74518dae1
tomghaines <tomhaines001@gmail.com>
2024-08-20 14:56:11 +0100

    HEATHROW Airport test data created as new Runway class

--- src/airspace.py
+++ src/airspace.py
@@ -1,4 +1,4 @@
-from classes import AltitudeBand, AltitudeBands, NewAirspace
+from classes import AltitudeBand, AltitudeBands, NewAirspace, Runway
 
 # Altitude Bands:
 alt_b_low = AltitudeBand(min_alt=0, max_alt=50)
@@ -7,8 +7,16 @@ alt_b_high = AltitudeBand(min_alt=250, max_alt=500)
 
 alt_bands = AltitudeBands(alt_band_low=alt_b_low, alt_band_mid=alt_b_mid, alt_band_high=alt_b_high)
 
+# Runway 09L/27R
+RunwayOne = Runway(name="Runway 09L", dimentions="12799 x 164 feet / 3901 x 50 meters", surface="HARD", coordinates="N51°28.65' / W0°29.10'", elevation=79, runway_heading=0.89, displaced_threshold=1014)
+RunwayTwo = Runway(name="Runway 27R", dimentions="12799 x 164 feet / 3901 x 50 meters", surface="HARD", coordinates="N51°28.66' / W0°26.00'", elevation=78, runway_heading=269, displaced_threshold=1014)
+
+# Runway 09R/27L
+RunwayOne = Runway(name="Runway 09R", dimentions="12799 x 164 feet / 3901 x 50 meters", surface="UNKNOWN", coordinates="N51°27.89' / W0°28.94'", elevation=75, runway_heading=0.89, displaced_threshold=1010)
+RunwayTwo = Runway(name="Runway 27L", dimentions="12799 x 164 feet / 3901 x 50 meters", surface="UNKNOWN", coordinates="N51°27.90' / W0°26.05'", elevation=77, runway_heading=269, displaced_threshold=1010)
+
 # Create new Airspace instance:
-TestAirspace = NewAirspace(name="TEST", lat=20, long=34, alt=50, alt_bands=alt_bands)
+TestAirspace = NewAirspace(name="HEATHROW", lat=20, long=34, alt=50, alt_bands=alt_bands)
 
 print(TestAirspace)
 
