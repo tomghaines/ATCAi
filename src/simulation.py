b3c6ddd6e022e8dcab1cd0341b23f95e70af2f0e
tomghaines <tomhaines001@gmail.com>
2024-08-20 17:19:15 +0100

    departure simulation logic added to Simulation class

--- src/simulation.py
+++ src/simulation.py
@@ -1,5 +1,8 @@
 import datetime
 import math
+from classes import NewAircraft, NewAirspace
+from airspace import TestAirspace
+from aircraft import TestAircraft
 
 DateTime = datetime.datetime.now()
 
@@ -14,3 +17,16 @@ class NewAircraft:
 
     def update_altitude(self, rate_of_climb_or_descent: float, time_elapsed: float):
         self.current_alt += rate_of_climb_or_descent * time_elapsed
+
+class Simulation:
+    def __init__(self, airspace, aircraft):
+        self.airspace = airspace
+        self.aircraft = aircraft
+    
+    def simulate_departure(self):
+        self.aircraft.status = "departing"
+        while self.aircraft.current_alt < self.airspace.alt_bands.alt_band_mid.min_alt:
+            self.aircraft.update_position(time_elapsed=1) # 1 unit of time
+            self.aircraft.update_altitude(rate_of_climb_or_descent=3000, time_elapsed=1) # 3000 ft/min
+            print(f"Aircraft {self.aircraft.aircraft_type} departing, current altitude: {self.current_alt}")
+        self.aircraft.status = "in-flight"
