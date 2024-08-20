7204828107acad1482168929a2ebc419ff16fe3a
tomghaines <tomhaines001@gmail.com>
2024-08-20 15:59:36 +0100

    typo fixed in class

--- src/classes.py
+++ src/classes.py
@@ -1,7 +1,27 @@
 from dataclasses import dataclass
 from typing import Tuple
 
-# Airspace
+@dataclass
+class AltitudeBand:
+    min_alt: float
+    max_alt: float
+
+@dataclass
+class AltitudeBands:
+    alt_band_low: AltitudeBand
+    alt_band_mid: AltitudeBand
+    alt_band_high: AltitudeBand
+
+@dataclass
+class NewRunway:
+    name: str
+    dimensions: str
+    surface: str
+    coordinates: str
+    elevation: float
+    runway_heading: float
+    displaced_threshold: float
+
 @dataclass
 class NewAirspace:
     name: str
@@ -12,26 +32,3 @@ class NewAirspace:
     runway: Tuple[NewRunway, NewRunway, NewRunway, NewRunway]
     coordinates: str
     elevation: float
-
-# Altitude Bands:
-@dataclass
-class AltitudeBands:
-    alt_band_low: AltitudeBand
-    alt_band_mid: AltitudeBand
-    alt_band_high: AltitudeBand
-
-@dataclass
-class AltitudeBand:
-    min_alt: float
-    max_alt: float
-
-# Runway
-@dataclass
-class NewRunway:
-    name: str
-    dimentions: str
-    surface: str
-    coordinates: str
-    elevation: float
-    runway_heading: float
-    displaced_threshold: int
