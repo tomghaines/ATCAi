38307763070e5835f55d26e83ddd532867a973e4
tomghaines <tomhaines001@gmail.com>
2024-08-22 13:52:48 +0100

    test_aircraft added

--- src/test_airspace.py
+++ src/test_airspace.py
@@ -14,3 +14,53 @@ runway_one = NewRunway(
     runway_heading=0.89,
     displaced_threshold=1014
 )
+
+runway_two = NewRunway(
+    name="Runway 27R",
+    dimensions="12799 x 164 feet / 3901 x 50 meters",
+    surface="HARD",
+    coordinates="N51°28.66' / W0°26.00'",
+    elevation=78,
+    runway_heading=269,
+    displaced_threshold=1014
+)
+
+runway_three = NewRunway(
+    name="Runway 09R",
+    dimensions="12799 x 164 feet / 3901 x 50 meters",
+    surface="UNKNOWN",
+    coordinates="N51°27.89' / W0°28.94'",
+    elevation=75,
+    runway_heading=0.89,
+    displaced_threshold=1010
+)
+
+runway_four = NewRunway(
+    name="Runway 27L",
+    dimensions="12799 x 164 feet / 3901 x 50 meters",
+    surface="UNKNOWN",
+    coordinates="N51°27.90' / W0°26.05'",
+    elevation=77,
+    runway_heading=269,
+    displaced_threshold=1010
+)
+
+
+# Define an aircraft
+def get_mach(v):
+    c = 343  # Speed of sound in m/s at sea level
+    return v / c
+
+test_aircraft = NewAircraft(
+    aircraft_type="Boeing 737 MAX 8-200 (B38M)",
+    registration="EI-IHT",
+    track=77,
+    airspeed=502,  # Example airspeed in knots
+    ground_speed=502,
+    mach=get_mach(502),
+    current_lat=51.4706,
+    current_long=-0.461941,
+    current_alt=0,
+    heading=90,  # Example heading in degrees
+    status="on-ground"
+)
