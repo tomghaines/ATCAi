7cdb3d7cb1180ffb342bc04f84c52cb82f1aabc9
tomghaines <tomhaines001@gmail.com>
2024-08-22 13:22:34 +0100

    Simulation class added & simulate_departure function added

--- src/simulation.py
+++ src/simulation.py
@@ -26,3 +26,20 @@ class NewAircraft:
 
     def update_altitude(self, rate_of_climb_or_decent: float, time_elapsed: float):
         self.current_alt += rate_of_climb_or_decent * time_elapsed
+
+class Simulation:
+    def __init__(self, airspace, aircraft):
+        self.airspace = airspace
+        self.aircraft = aircraft
+    
+    def simulate_departure(self):
+        self.aircraft.status = "departing"
+        while self.aircraft.current_alt < self.airspace.alt_bands.alt_band_mid.min_alt:
+            self.aircraft.update_position(time_elapsed=1) # 1 unit of time
+            self.aircraft.update_altitude(rate_of_climb_or_decent=3000/60, time_elapsed=1) # 3000 ft/min, converted to ft/second
+
+            # Output the current state
+            print(f"Aircraft {self/aircraft.aircraft_type} departing, current altitude: {self.current_alt:.2f} feet, position: ({self.aircraft.current_lat:.4f}, {self.aircraft.current_long:.4f})")
+
+        self.aircraft.status = "in-flight"
+        print(f"Aircraft {self.aircraft.aircraft_type} has reached cruising altitude, status: {self.aircraft.status}")
