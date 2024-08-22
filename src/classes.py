dd203c18173a18e3d6877052354ad1d1203fde06
tomghaines <tomhaines001@gmail.com>
2024-08-22 14:01:11 +0100

    update_position & update_altitude functions added to NewAircraft class

--- src/classes.py
+++ src/classes.py
@@ -48,3 +48,14 @@ class NewAircraft:
     current_alt: float
     heading: float
     status: str
+    
+    def update_position(self, time_elapsed: float):
+        distance_traveled = self.ground_speed * time_elapsed
+        delta_lat = distance_traveled * math.cos(math.radians(self.heading))
+        delta_long = distance_traveled * math.sin(math.radians(self.heading))
+
+        self.current_lat += delta_lat
+        self.current_long += delta_long
+
+    def update_altitude(self, rate_of_climb_or_descent: float, time_elapsed: float):
+        self.current_alt += rate_of_climb_or_descent * time_elapsed
